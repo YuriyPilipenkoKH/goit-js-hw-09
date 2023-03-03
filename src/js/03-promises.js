d94e2563774bd178ebd66d1db8b3dbf5cbffaff3
Yuriy Pilipenko <yurik2061@gmail.com>
2023-03-03 14:32:01 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,7 +8,7 @@ const createBtn = document.querySelector('form > button')
 
 form.addEventListener('submit', onFormSubmit);
 
-function createPromise(position, delay) {
+function createPromise(position, delay,amount ) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
@@ -19,15 +19,17 @@ function createPromise(position, delay) {
         reject({ position, delay });
         // Reject
       }
-  
-      
+
+      if(position === amount) {
+        createBtn.removeAttribute('disabled', '')
+      }
     }, delay);
   });
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-
+  createBtn.setAttribute('disabled', '')
 
   const formData = new FormData(e.currentTarget);
 
@@ -41,7 +43,7 @@ function onFormSubmit(e) {
 
   for (let position = 1; position <= amount; position += 1) {
    
-    createPromise(position, delay)
+    createPromise(position, delay,amount)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
@@ -55,8 +57,8 @@ function onFormSubmit(e) {
    
     delay += step;
   
-   
+    
   }
   e.currentTarget.reset();
-
+  
 }
