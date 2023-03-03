141dfae05d2032f71b4855287df93121259f40dd
Yuriy Pilipenko <yurik2061@gmail.com>
2023-03-03 13:17:14 +0200

    console.log(

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-
+console.log('dart');
 let timerId = null;
 let formValues = {};
 
@@ -38,6 +38,7 @@ function onFormSubmit(e) {
   let amount = Number(formValues.amount);
 
   for (let position = 1; position <= amount; position += 1) {
+   
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
@@ -51,6 +52,7 @@ function onFormSubmit(e) {
       })
    
     delay += step;
+    console.log(position);
    
   }
   e.currentTarget.reset();
